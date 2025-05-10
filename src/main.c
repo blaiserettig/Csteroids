a573c14e80e2ab8660d94943ec4c6eff9d92c991
Alx-R <91144847+Alx-R@users.noreply.github.com>
2025-05-09 23:12:44 -0700

    window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
 #include "SDL3/SDL.h"
-#include <stdio.h>
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_video.h"
+
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 400;
 
 int main(int argc, char* argv[]) {
-    printf("Hello World!");
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL_Init Error: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Csteroids", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if (!window) {
+        SDL_Log("SDL_CreateWindow Error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(10000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
