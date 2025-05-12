a5efd836a5df90e8ac9330d4b8db581ff7461219
Alx-R <91144847+Alx-R@users.noreply.github.com>
2025-05-12 00:46:54 -0700

    asteroidssss

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_render.h"
 #include "SDL3/SDL_scancode.h"
-#include "SDL3/SDL_timer.h"
 #include "SDL3/SDL_video.h"
 #include <stdlib.h>
 
@@ -19,15 +18,12 @@
 
 typedef uint32_t u32;
 
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 400
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 #define THRUST 0.5
 
-typedef struct {
-    float x;
-    float y;
-} v2;
+#define MAX_ASTEROIDS 32
 
 typedef struct {
     v2 position;
@@ -43,6 +39,12 @@ struct {
     u32 pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
     bool quit;
     ship ship;
+    small_asteroid small_asteroids[MAX_ASTEROIDS];
+    int small_asteroid_count;
+    medium_asteroid medium_asteroids[MAX_ASTEROIDS];
+    int medium_asteroid_count;
+    large_asteroid large_asteroids[MAX_ASTEROIDS];
+    int large_asteroid_count;
     int w;
     int a;
     int d;
@@ -60,8 +62,8 @@ v2 ship_points[] = {
 
 int main(int argc, char* argv[]) {
 
-    state.ship.position.x = SCREEN_WIDTH / 2;
-    state.ship.position.y = SCREEN_HEIGHT / 2;
+    state.ship.position.x = SCREEN_WIDTH / 2.0;
+    state.ship.position.y = SCREEN_HEIGHT / 2.0;
     state.ship.velocity.x = 0;
     state.ship.velocity.y = 0;
     state.ship.angle = 0;
@@ -107,6 +109,14 @@ int main(int argc, char* argv[]) {
 void update() {
     handle_input();
 
+    update_ship();
+    update_asteroids();
+
+    render_ship();
+    render_asteroids();
+}
+
+void update_ship() {
     state.ship.position.x = wrap(state.ship.position.x + state.ship.velocity.x, SCREEN_WIDTH);
     state.ship.position.y = wrap(state.ship.position.y + state.ship.velocity.y, SCREEN_HEIGHT);
 
@@ -127,8 +137,21 @@ void update() {
 
     state.ship.velocity.x = clamp(state.ship.velocity.x, -10, 10);
     state.ship.velocity.y = clamp(state.ship.velocity.y, -10, 10);
+}
 
-    render_ship();
+void update_asteroids() {
+    for (int i = 0; i < state.small_asteroid_count; i++) {
+        state.small_asteroids[i].position.x = wrap(state.small_asteroids[i].position.x += state.small_asteroids[i].velocity.x, SCREEN_WIDTH);
+        state.small_asteroids[i].position.y = wrap(state.small_asteroids[i].position.y += state.small_asteroids[i].velocity.y, SCREEN_HEIGHT);
+    }
+    for (int i = 0; i < state.medium_asteroid_count; i++) {
+        state.medium_asteroids[i].position.x = wrap(state.medium_asteroids[i].position.x += state.medium_asteroids[i].velocity.x, SCREEN_WIDTH);
+        state.medium_asteroids[i].position.y = wrap(state.medium_asteroids[i].position.y += state.medium_asteroids[i].velocity.y, SCREEN_HEIGHT);
+    }
+    for (int i = 0; i < state.large_asteroid_count; i++) {
+        state.large_asteroids[i].position.x = wrap(state.large_asteroids[i].position.x += state.large_asteroids[i].velocity.x, SCREEN_WIDTH);
+        state.large_asteroids[i].position.y = wrap(state.large_asteroids[i].position.y += state.large_asteroids[i].velocity.y, SCREEN_HEIGHT);
+    }
 }
 
 void handle_input() {
@@ -148,6 +171,11 @@ void handle_input() {
                         break;
                     case SDL_SCANCODE_A:
                         state.a = 1;
+                        break;
+                    case SDL_SCANCODE_F:
+                        generate_small_asteroid();
+                        generate_large_asteroid();
+                        generate_medium_asteroid();
                         break;
                     default:
                         break;
@@ -190,6 +218,102 @@ void render_ship() {
     }
 }
 
+void render_asteroids() {
+    for (int i = 0; i < state.small_asteroid_count; i++) {
+        render_asteroids_helper(state.small_asteroids[i].position, state.small_asteroids[i].points, 9);
+    }
+    for (int i = 0; i < state.medium_asteroid_count; i++) {
+        render_asteroids_helper(state.medium_asteroids[i].position, state.medium_asteroids[i].points, 11);
+    }
+    for (int i = 0; i < state.large_asteroid_count; i++) {
+        render_asteroids_helper(state.large_asteroids[i].position, state.large_asteroids[i].points, 12);
+    }
+}
+
+void render_asteroids_helper(v2 pos, v2* points, int p_count) {
+    for (int i = 0; i < p_count; i++) {
+        int next = (i + 1) % p_count;
+        SDL_RenderLine(state.renderer, pos.x + points[i].x, pos.y + points[i].y, pos.x + points[next].x, pos.y + points[next].y);
+    }
+}
+
+void generate_small_asteroid() {
+
+    if (state.small_asteroid_count >= MAX_ASTEROIDS)
+        return;
+
+    small_asteroid small = {
+        .position = {SCREEN_WIDTH / 2.0 + 50.0, SCREEN_HEIGHT / 2.0 + 50.0},
+        .velocity = {randf(-2.0, 2.0), randf(-2.0, 2.0)},
+        .angle = 0,
+        .points = {
+            {-4, 0},
+            {-5, 2},
+            {-3, 4},
+            {-1, 3},
+            {2, 5},
+            {5, 1},
+            {4, -4},
+            {-1, -5},
+            {-5, -3}
+        }
+    };
+    state.small_asteroids[state.small_asteroid_count++] = small;
+}
+
+void generate_medium_asteroid() {
+
+    if (state.medium_asteroid_count >= MAX_ASTEROIDS)
+        return;
+
+    medium_asteroid medium = {
+        .position = {SCREEN_WIDTH / 2.0 - 100.0, SCREEN_HEIGHT / 2.0 - 100.0},
+        .velocity = {randf(-1.5, 1.5), randf(-1.5, 1.5)},
+        .angle = 0,
+        .points = {
+            {-2, -1},
+            {-7, -7},
+            {-8, -2},
+            {-5, -1},
+            {-8, 3},
+            {-3, 8},
+            {3, 8},
+            {8, 5},
+            {8, -3},
+            {1, -9},
+            {-2, -9}
+        }
+    };
+    state.medium_asteroids[state.medium_asteroid_count++] = medium;
+}
+
+void generate_large_asteroid() {
+
+    if (state.large_asteroid_count >= MAX_ASTEROIDS)
+        return;
+
+    large_asteroid large = {
+        .position = {SCREEN_WIDTH / 2.0 + 150.0, SCREEN_HEIGHT / 2.0 - 150.0},
+        .velocity = {randf(-1.0, 1.0), randf(-1.0, 1.0)},
+        .angle = 0,
+        .points = {
+            {-11, -1},
+            {-13, 7},
+            {-5, 14},
+            {1, 12},
+            {6, 14},
+            {13, 8},
+            {8, 5},
+            {15, -1},
+            {9, -11},
+            {-3, -13},
+            {-5, -17},
+            {-14, -8}
+        }
+    };
+    state.large_asteroids[state.large_asteroid_count++] = large;
+}
+
 float wrap(float given, float max) {
     if (given < 0) return max;
     if (given > max) return 0;
@@ -203,4 +327,9 @@ void apply_friction(float *v, float amount) {
 
 float clamp(float val, float min, float max) {
     return fmaxf(min, fminf(max, val));
+}
+
+float randf(float min, float max) {
+    float scale = rand() / (float) RAND_MAX;
+    return min + scale * (max - min);
 }
--- src/main.h
+++ src/main.h
@@ -1,11 +1,54 @@
+typedef struct {
+    float x;
+    float y;
+} v2;
+
+typedef struct {
+    v2 position;
+    v2 velocity;
+    float angle;
+    v2 points[9]; 
+} small_asteroid;
+
+typedef struct {
+    v2 position;
+    v2 velocity;
+    float angle;
+    v2 points[11];
+} medium_asteroid;
+
+typedef struct {
+    v2 position;
+    v2 velocity;
+    float angle;
+    v2 points[12];
+} large_asteroid;
+
+
 void render_ship();
 
 void handle_input();
 
 void update();
 
+void update_ship();
+
+void update_asteroids();
+
 float wrap(float given, float max);
 
 void apply_friction(float *v, float amount);
 
 float clamp(float val, float min, float max);
+
+void render_asteroids();
+
+void render_asteroids_helper(v2 pos, v2* points, int p_count);
+
+void generate_small_asteroid();
+
+void generate_medium_asteroid();
+
+void generate_large_asteroid();
+
+float randf(float min, float max);
